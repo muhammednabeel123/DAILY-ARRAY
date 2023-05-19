5b909bd44225feb40455d7c8c8556e27b866ed9e
muhammednabeel123 <muhammednabeel237@gmail.com>
2023-05-19 11:31:28 +0530

    with  updateed

--- LeatCodeFindingSquareOfAnotherArray.js
+++ LeatCodeFindingSquareOfAnotherArray.js
@@ -1,3 +1,5 @@
+
+
 function Square(arr1,arr2){
     count = 0
     let arr3=[]
@@ -7,7 +9,7 @@ function Square(arr1,arr2){
             if(arr1.length === arr2.length ){
                 if ( arr1[i] * arr1[i] === arr2[j]) {
                     arr3.push(arr1[i])
-                    arr1[j] = -1
+                    arr2[j] = -1
                 }
               
                 
@@ -19,4 +21,31 @@ function Square(arr1,arr2){
     return arr3
 }
 
-console.log(Square([1,2,3,],[1,4,9]))
+//complexity O(n^2)
+
+console.log(Square([1,2,3],[4,1,9]))
+
+// using while loop
+
+function Square2(arr1,arr2){
+    let arr3 = []
+    let i=0
+    let j=0
+
+
+    if(arr1.length === arr2.length){
+        while(i < arr2.length && j < arr1.length){
+            if(arr1[j] != -1 && arr1[i]*arr1[i] === arr2[j] ){
+                arr3.push(arr1[i])
+                arr1[j] = -1
+                i++,j=0
+            }
+            else{
+                j++
+            }
+        }
+        return arr3
+    }
+    
+}
+
